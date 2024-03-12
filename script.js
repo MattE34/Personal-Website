a355c826921609161dc916949f825319b9f9bcf2
MattE34 <mesports16@gmail.com>
2024-03-12 13:10:59 -0400

    partial navbar

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ resume.addEventListener("click", function() {
     window.open("media/Resume.pdf", "_blank")
 })
 
-  
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
