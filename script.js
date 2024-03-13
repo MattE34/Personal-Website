dfbd7a3d2e2425f0980076bad3716bf4982674fd
MattE34 <mesports16@gmail.com>
2024-03-13 18:45:16 -0400

    project heading photos

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ resume.addEventListener("click", function() {
 })
 
 window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > 0) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-  });
+  var navbar = document.getElementById('navbar');
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
