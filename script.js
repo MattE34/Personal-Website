714acaaedddaacee5a37f4a7b5cce9f5461a54bf
MattE34 <mesports16@gmail.com>
2024-03-14 16:39:42 -0400

    modified bio and cleaned code

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-let resume = document.getElementById('resumeFile');
+let resume = document.getElementById("resumeFile");
 
-resume.addEventListener("click", function() {
-    window.open("media/Resume.pdf", "_blank")
-})
+resume.addEventListener("click", function () {
+  window.open("media/Resume.pdf", "_blank");
+});
 
-window.addEventListener('scroll', function() {
-  var navbar = document.getElementById('navbar');
+window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("navbar");
   if (window.scrollY > 0) {
-    navbar.classList.add('scrolled');
+    navbar.classList.add("scrolled");
   } else {
-    navbar.classList.remove('scrolled');
+    navbar.classList.remove("scrolled");
   }
 });
 
-
-
-ScrollReveal( {reset: true,
+ScrollReveal({
+  reset: true,
   // setting distance >= '120px' causes a horizontal bar at the bottom of the page to appear
-  distance: '100px',
+  distance: "100px",
   duration: 1500,
   delay: 100,
-  opacity: 0});
+  opacity: 0,
+});
 
-ScrollReveal().reveal('.home-content h1, .about-content h2, .skills-content h2, .techs, .projects-content h2', {origin: 'top'});
-ScrollReveal().reveal('.home-content p, .self-image', {origin: 'left'});
-ScrollReveal().reveal('.socials, .activities, .bio', {origin: 'right'});
-ScrollReveal().reveal('.whoButton, .description, .products', {origin: 'bottom'});
+ScrollReveal().reveal(
+  ".home-content h1, .about-content h2, .skills-content h2, .techs, .projects-content h2",
+  { origin: "top" }
+);
+ScrollReveal().reveal(".home-content p, .self-image", { origin: "left" });
+ScrollReveal().reveal(".socials, .activities, .bio", { origin: "right" });
+ScrollReveal().reveal(".whoButton, .description, .products", {
+  origin: "bottom",
+});
